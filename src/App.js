3c43431dc75cd301a677587d41277d1b9b40c0a5
Anarbek Niiazakunov <anarbek.94st@gmail.com>
2022-08-26 03:34:58 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
+  const element = {principal:"blue"}
+  const nameUsers =[ " Zama", " Dada","Meerim"]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          nameUsers.map((user) =><Header nameUser={user} greeting=' good bye' tacos=' beans' Color={element} />)
+        }
+        {/* <Header 
+        nameUser=" Zama" 
+        greeting=' good bye'
+        tacos=' beans'
+        Color={element}
+        />
+        <Header 
+        nameUser=" Dada" 
+        greeting=' good bye'
+        tacos=' beans'
+        Color={element}
+        /> */}
+        
       </header>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header({greeting,nameUser}){
+    // console.log(props)
+
+    return (
+    <h1>Hello React{greeting} {nameUser}</h1>
+    )
+}
+export default Header;
